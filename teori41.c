a0111bee80a3fef31dfb22719eb285e31f85d83e
Es Kulkul <mobilterbang537@gmail.com>
2025-09-26 23:27:32 +0700

    Add files via upload

---
+++ teori41.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int nilai_test = 78;
+    if(nilai_test >= 75){
+        printf("Anda lulus!");
+    }
+    else{
+        printf("Anda tidak lulus!");
+    }
+    return 0;
+}
